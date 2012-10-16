24292880f7a84bef6d30b01241508a19a6ee2179
Sergey <sergredko@gmail.com>
2012-10-16 14:12:06 +0200

    fix setup script

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "WARNING: Must be launched from the same directory of emacs-main.el" 
-echo ";; call .emacs from source control" > ~/.emacs
-echo "(add-to-list 'load-path \"`echo $PWD | sed -E -e 's@'$HOME'(.*)@~\1@g'`\")" >> ~/.emacs
-echo "(load \"emacs-main\")" >> ~/.emacs
-
-
